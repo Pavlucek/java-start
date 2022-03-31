34890d7efd09b5477a7c64a1f6fdfcfdc677d159
Pavlucek <p.morozewicz99@gmail.com>
2022-03-31 12:51:35 +0200

    zmiana nastepna

--- src/klasa.java
+++ src/klasa.java
@@ -1,8 +1,18 @@
+
 public class klasa {
-    public klasa() {
-    }
 
     public static void main(String[] args) {
-        System.out.println("Jazda");
+
+        String imie = "Pawel";
+        String Nazwisko = "Morozewicz";
+        int wiek = 23;
+
+        String DaneOsobowe = imie + " " + Nazwisko + " " + wiek;
+
+        final double ZMIENNA_PI = 3.14;
+
+        System.out.println(ZMIENNA_PI);
+
+        System.out.println(DaneOsobowe);
     }
 }
